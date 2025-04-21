7437941dd7590c0d80a7af2f279b49224acee79a
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-04-22 01:25:55 +0600

    edit profile updated, now age and gender can be added too

--- backend/src/modules/users/controller/updateProfile.ts
+++ backend/src/modules/users/controller/updateProfile.ts
@@ -4,23 +4,33 @@ import { UpdateProfileZodSchema } from "../../../validators/user.validator";
 
 const updateProfile = async (req: any, res: Response, next: NextFunction) => {
   try {
-    const parsed = UpdateProfileZodSchema.safeParse(req.body);
-
-    if (!parsed.success) {
-      res.status(400).json({ errors: parsed.error.flatten().fieldErrors });
+    const { success, data, error } = UpdateProfileZodSchema.safeParse(req.body);
+    if (!success) {
+      res.status(400).json({ message: error.errors[0].message });
       return;
     }
 
-    const updatedUser = await User.findByIdAndUpdate(
-      req.user._id,
-      { $set: parsed.data },
-      { new: true, runValidators: true }
-    ).select("-password");
+    const userId = req.user?._id;
+    const user = await User.findById(userId);
+    if (!user) throw "User not found";
+
+    Object.assign(user, data); // updates only provided fields
+    await user.save();
 
     res.status(200).json({
-      message: "Profile Updated Successfully",
-      updatedUser,
+      message: "Profile updated successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        isBanned: user.isBanned,
+        image: user.image,
+        address: user.address,
+        gender: user.gender,
+        dateOfBirth: user.dateOfBirth,
+      },
     });
+    return;
   } catch (error) {
     next(error);
   }
