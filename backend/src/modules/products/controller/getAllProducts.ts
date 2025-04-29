d0f777bc5e3f7a939ee6090212da936985e09900
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-04-30 01:26:21 +0600

    updated slightly

--- backend/src/modules/products/controller/getAllProducts.ts
+++ backend/src/modules/products/controller/getAllProducts.ts
@@ -1,11 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import Product from "../../../models/product.model";
 import catchAsync from "../../../utils/catchAsync";
+import { AuthRequest } from "../../../middlewares/authMiddleware";
 
 const getAllProducts = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const products = await Product.find({isDeleted: false }); //remove soft deleted products
-
+  async (req: AuthRequest, res: Response, next: NextFunction) => {
+    const products = await Product.find(); 
     res.status(200).json({
       status: "Success",
       totalProducts: products.length,
