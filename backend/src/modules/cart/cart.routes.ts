dd7efa52e29ca5998b96c1809cd0a1153f9382d3
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-05-13 20:23:28 +0600

    now buyer can remove an item from cart by query parameter the product id

--- backend/src/modules/cart/cart.routes.ts
+++ backend/src/modules/cart/cart.routes.ts
@@ -3,13 +3,15 @@ import { addToCart } from './controller/addToCart';
 import auth from '../../middlewares/authMiddleware';
 import authorize from '../../middlewares/authorize';
 import getMyCart from './controller/getMyCart';
+import removeFromCart from './controller/removeFromCart';
 
-const cartRoures = Router();
+const cartRoutes = Router();
 
 
-cartRoures.use(auth);
-cartRoures.use(authorize("buyer"));
-cartRoures.post('/add', addToCart);
-cartRoures.get('/my-cart', getMyCart);
+cartRoutes.use(auth);
+cartRoutes.use(authorize("buyer"));
+cartRoutes.post('/add', addToCart);
+cartRoutes.get('/my-cart', getMyCart);
+cartRoutes.delete("/remove/:productId", removeFromCart);
 
-export default cartRoures;
+export default cartRoutes;
