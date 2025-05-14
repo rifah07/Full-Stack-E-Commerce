7e472890bd84bfa6889afdacc9637ab4deda4b50
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-05-14 23:40:08 +0600

    helmet installed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
