686a6612209853e314e7ce0c7ec7f2549ba7a09f
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-05-15 21:15:43 +0600

    cart route updated with requset body and api parameter validation

--- backend/src/modules/cart/cart.routes.ts
+++ backend/src/modules/cart/cart.routes.ts
@@ -1,19 +1,73 @@
-import { Router } from 'express';
-import { addToCart } from './controller/addToCart';
-import auth from '../../middlewares/authMiddleware';
-import authorize from '../../middlewares/authorize';
-import getMyCart from './controller/getMyCart';
-import removeFromCart from './controller/removeFromCart';
-import updateCartItemQuantity from './controller/updateCartItemQuantity';
+import { Router } from "express";
+import { addToCart } from "./controller/addToCart";
+import auth from "../../middlewares/authMiddleware";
+import authorize from "../../middlewares/authorize";
+import getMyCart from "./controller/getMyCart";
+import removeFromCart from "./controller/removeFromCart";
+import updateCartItemQuantity from "./controller/updateCartItemQuantity";
+import { body, param, validationResult } from "express-validator";
+import { Request, Response, NextFunction } from "express";
+import { BadRequestError } from "../../utils/errors";
 
 const cartRoutes = Router();
 
+//middleware to handle validation errors
+const validate = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new BadRequestError(
+        "Validation failed: " + JSON.stringify(errors.array())
+      )
+    );
+  }
+  next();
+};
 
 cartRoutes.use(auth);
 cartRoutes.use(authorize("buyer"));
-cartRoutes.post('/add', addToCart);
-cartRoutes.get('/my-cart', getMyCart);
-cartRoutes.delete("/remove/:productId", removeFromCart);
-cartRoutes.patch("/update/:productId", updateCartItemQuantity);
+
+cartRoutes.post(
+  "/add",
+  [
+    body("productId")
+      .notEmpty()
+      .isMongoId()
+      .withMessage("Product ID must be a valid MongoDB ID."),
+    body("quantity")
+      .isInt({ min: 1 })
+      .withMessage("Quantity must be a positive integer."),
+  ],
+  validate,
+  addToCart
+);
+
+cartRoutes.get("/my-cart", getMyCart);
+
+cartRoutes.delete(
+  "/remove/:productId",
+  [
+    param("productId")
+      .notEmpty()
+      .isMongoId()
+      .withMessage("Product ID must be a valid MongoDB ID."),
+  ],
+  validate,
+  removeFromCart
+);
+cartRoutes.patch(
+  "/update/:productId",
+  [
+    param("productId")
+      .notEmpty()
+      .isMongoId()
+      .withMessage("Product ID must be a valid MongoDB ID."),
+    body("quantity")
+      .isInt({ min: 1 })
+      .withMessage("Quantity must be a positive integer."),
+  ],
+  validate,
+  updateCartItemQuantity
+);
 
 export default cartRoutes;
