1b55a2d196ba2a1a089e76807dfb2cf0dce3e6f1
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-05-15 21:42:20 +0600

    wishlist route updated with requset body and api parameter validation

--- backend/src/modules/wishlist/wishlist.routes.ts
+++ backend/src/modules/wishlist/wishlist.routes.ts
@@ -4,15 +4,53 @@ import authorize from "../../middlewares/authorize";
 import addToWishlist from "./controller/addToWishlist";
 import getUserWishlist from "./controller/getUserWishlist";
 import removeFromWishlist from "./controller/removeFromWishlist";
+import { body, param, validationResult } from "express-validator";
+import { Request, Response, NextFunction } from "express";
+import { BadRequestError } from "../../utils/errors";
 
 const wishlistRoutes = express.Router();
+
+//middleware to handle validation errors
+const validate = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new BadRequestError(
+        "Validation failed: " + JSON.stringify(errors.array())
+      )
+    );
+  }
+  next();
+};
 
 //protected Routes
 wishlistRoutes.use(auth);
 wishlistRoutes.use(authorize("buyer"));
 
-wishlistRoutes.post("/add", addToWishlist);
+wishlistRoutes.post(
+  "/add",
+  [
+    body("productId")
+      .notEmpty()
+      .isMongoId()
+      .withMessage("Product ID must be a valid MongoDB ID."),
+  ],
+  validate,
+  addToWishlist
+);
+
 wishlistRoutes.get("/", getUserWishlist);
-wishlistRoutes.delete("/remove/:productId", removeFromWishlist);
+
+wishlistRoutes.delete(
+  "/remove/:productId",
+  [
+    param("productId")
+      .notEmpty()
+      .isMongoId()
+      .withMessage("Product ID must be a valid MongoDB ID."),
+  ],
+  validate,
+  removeFromWishlist
+);
 
 export default wishlistRoutes;
